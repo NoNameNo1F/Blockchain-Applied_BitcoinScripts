f2ad5e18417ac47f701de21947cd8914983987ff
20127670 <nonameno1f@gmail.com>
2024-07-29 02:34:16 +0700

    feat: update main

--- src/main.py
+++ src/main.py
@@ -1,27 +1,7 @@
-from datetime import datetime
+from btc import task1_p2pkh, task2_p2sh
 
-from bitcoinutils.keys import P2pkhAddress, PrivateKey
-from bitcoinutils.script import Script
-from bitcoinutils.setup import setup
-from bitcoinutils.transactions import Transaction, TxInput, TxOutput
-from bitcoinutils.utils import to_satoshis
-
-
-def log(log_type: str, message: str):
-    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    msg = f"[{timestamp}] - [{log_type.upper()}]: {message}"
-    print(msg)
-    
 if __name__ == "__main__":
-    setup("testnet")
-    # Generate a random private key
-    private_key = PrivateKey()
-
-    # Derive the public key and Bitcoin address
-    public_key = private_key.get_public_key()
-    address = P2pkhAddress(public_key.get_address().to_string())
-
-    log("info",f"Private Key: {private_key.to_wif()}")
-    log("info",f"Public Key: {public_key.to_hex()}")
-    log("info",f"Bitcoin Address: {address.to_string()}")
-    
+    print("Task 1 : Pay - to - PublicKeyHash")
+    task1_p2pkh()
+    print("Task 2 : Pay - to - MultiSig")
+    task2_p2sh()
